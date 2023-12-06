c0daa7f2aaee8fc3fd617ecc29a0c39c1e883d48
osuke-noguchi <noguchi.nodai@gmail.com>
2023-12-07 05:45:48 +0900

    moleculesの作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
+import { SearchInput } from './components/molecules/SearchInput';
 
 function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput></SearchInput>
     </div>
   );
 }
